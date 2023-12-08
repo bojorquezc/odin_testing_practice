aaf3f9246b57169dc61177d3c66ccb9e2c0f2ece
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-12-07 20:53:18 -0800

    Caesar cipher space & no space

--- functions.js
+++ functions.js
@@ -25,8 +25,64 @@ const calculator = {
     },
 }
 
+const alphabet = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+
+function findCharIndex(text) {
+    const array = Array.from(text);
+    const positionArray = [];
+
+    for (const char of array) {
+        if (alphabet.indexOf(char) == -1) {
+            positionArray.push(null);
+        } else {
+            positionArray.push(alphabet.indexOf(char));
+        }
+    }
+
+    return positionArray;
+}
+
+function shiftedArray (array, shiftFactor) {
+    const shiftedArray = [];
+
+    for (const position of array) {
+        if (position == null) {
+            shiftedArray.push(null)
+        } else {
+            shiftedArray.push(position + shiftFactor);
+        }
+    }
+
+    return shiftedArray;
+}
+
+function decipherArray(array) {
+    const decipheredArray = [];
+
+    for (const position of array) {
+        if (position == null) {
+            decipheredArray.push(' ')
+        } else {
+            decipheredArray.push(alphabet[position])
+        }
+    }
+    
+    return decipheredArray;
+}
+
+
+function caesarCipher (text, shiftFactor) {
+    const positionArray = findCharIndex(text);
+    const incrementArray = shiftedArray(positionArray, shiftFactor);
+    const decipheredArray = decipherArray(incrementArray).join("");
+    return decipheredArray;
+}
+
 export {
     capitalize,
     reverseString,
     calculator,
+    caesarCipher,
 }
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from "./functions";
+import { capitalize, reverseString, calculator, caesarCipher } from "./functions";
 
 test('capitalizes string', () => {
     expect(capitalize('hello world')).toBe('Hello world');
@@ -13,4 +13,10 @@ test('calculator results', () => {
     expect(calculator.subtract(2,2)).toBe(0);
     expect(calculator.divide(2,2)).toBe(1);
     expect(calculator.multiply(2,2)).toBe(4);
-})
+});
+
+test('caesar cipher', () => {
+    expect(caesarCipher('hello', 1)).toBe('ifmmp');
+    expect(caesarCipher('hello foo', 1)).toBe('ifmmp gpp');
+    expect(caesarCipher('hello', 5)).toBe('mjqqt');
+});
